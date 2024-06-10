9b3f477de398e84f70da6f91fe6ea1e6246ce2d4
jgarcia-2018163 <jgarcia-2018163@kinal.edu.gt>
2024-06-09 19:44:49 -0600

    create function generateTokem

---
+++ Back/src/services/jwt.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const jwt = require('jsonwebtoken')
+
+exports.generateToken = async (data) => {
+    try{
+        const payload = {
+            id: data._id,
+            email: data.email,
+            username: data.username,
+            //role: data.role
+        }
+    
+        const options = {
+            expiresIn: '2h'
+        }
+    
+        const token = jwt.sign(payload, process.env.SECRET_KEY, options)
+        return token
+    }catch(err){
+        console.log(err)
+        return err
+    }
+}
