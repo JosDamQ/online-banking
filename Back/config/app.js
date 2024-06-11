45aca122d5d021b0caba5d3bde65ce26e5279c3b
jgarcia-2018163 <jgarcia-2018163@kinal.edu.gt>
2024-06-11 15:33:48 -0600

    create route for login

--- Back/config/app.js
+++ Back/config/app.js
@@ -7,6 +7,8 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3200
 
+const adminController = require('../src/admins/admin.controller')
+
 // aqui van a ir las importaciones de las rutas que vaya a tener
 const userRoutes = require('../src/users/users.routes')
 const adminRoutes = require('../src/admins/admin.routes')
@@ -22,6 +24,7 @@ app.use(morgan('dev'));
 // aqui van a ir las rutas que vaya a tener
 app.use('/users', userRoutes)
 app.use('/admins', adminRoutes)
+app.post('/login', adminController.login)
 
 // Levantar el server
 exports.initServer = () => {
