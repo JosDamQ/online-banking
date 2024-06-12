941479d369c28bcc7af1b69bc9a0765859f42702
jgarcia-2018163 <jgarcia-2018163@kinal.edu.gt>
2024-06-11 18:46:47 -0600

    implements ajv

--- Back/src/admins/admin.routes.js
+++ Back/src/admins/admin.routes.js
@@ -6,9 +6,13 @@ const router = express.Router()
 const { verifyToken } = require('../middlewares/loggedIn')
 const { isAdmin } = require('../middlewares/isAdmin')
 
+// AJV
+const { validateBody, validateParams, validateQuery } = require('../middlewares/AJV_validate/index')
+const schemaCreateUser = require('../middlewares/AJV_validate/Admin/CreateUser')
+
 //Rutas
 router.post('/create-admin', verifyToken, isAdmin, adminController.createAdmin)
-router.post('/create-user', verifyToken, isAdmin, adminController.createUser)
+router.post('/create-user', verifyToken, isAdmin, validateBody(schemaCreateUser), adminController.createUser)
 
 //
 module.exports = router
