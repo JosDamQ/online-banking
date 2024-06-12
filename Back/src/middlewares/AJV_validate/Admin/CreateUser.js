4fde357d03b9823e2675acddf04c68a7a59c7ffa
jgarcia-2018163 <jgarcia-2018163@kinal.edu.gt>
2024-06-11 18:46:11 -0600

    create schema for api createUser

---
+++ Back/src/middlewares/AJV_validate/Admin/CreateUser.js
@@ -0,0 +1,156 @@
+const Ajv = require("ajv")
+const ajv = new Ajv({allErrors: true, allowUnionTypes:true})
+require("ajv-errors")(ajv)
+
+const schemaCreateUser = {
+  type: "object",
+  properties: {
+    name: {
+      type: "string",
+      minLength: 3,
+      maxLength: 50,
+      pattern: "^[a-zA-Z ]+$",
+      errorMessage: {
+        type: "Name must be a string",
+        minLength: "Name must be at least 1 character long",
+        maxLength: "Name must be at most 50 characters long",
+        pattern: "Name must only contain letters",
+      },
+    },
+    username: {
+      type: "string",
+      minLength: 3,
+      maxLength: 50,
+      pattern: "^[a-zA-Z0-9]+$",
+      errorMessage: {
+        type: "Username must be a string",
+        minLength: "Username must be at least 3 characters long",
+        maxLength: "Username must be at most 50 characters long",
+        pattern: "Username must only contain letters and numbers",
+      },
+    },
+    NoIdentification: {
+      type: ["string", "number"],
+      minLength: 13,
+      maxLength: 13,
+      pattern: "^[0-9]+$",
+      errorMessage: {
+        type: "NoIdentification must be a string or number",
+        minLength: "NoIdentification must be at least 13 characters long",
+        maxLength: "NoIdentification must be at most 13 characters long",
+        pattern: "NoIdentification must only contain numbers",
+      },
+    },
+    birthdate: {
+      type: "string",
+      pattern: "^\\d{4}-\\d{2}-\\d{2}$",
+      errorMessage: {
+        type: "El campo fecha_inicial debe ser de tipo string",
+        pattern: "El campo fecha_inicial debe tener el formato YYYY-MM-DD",
+      },
+    },
+    address: {
+      type: "string",
+      minLength: 3,
+      maxLength: 50,
+      errorMessage: {
+        type: "Address must be a string",
+        minLength: "Address must be at least 3 characters long",
+        maxLength: "Address must be at most 50 characters long",
+      },
+    },
+    phone: {
+      type: "number",
+      minLength: 8,
+      maxLength: 8,
+      errorMessage: {
+        type: "Phone must be a number",
+        minLength: "Phone must be 8 characters long",
+        maxLength: "Phone must be 8 characters long",
+      },
+    },
+    email: {
+      type: "string",
+      pattern: "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
+      errorMessage: {
+        type: "Email must be a string",
+        pattern: "Email must be in the format: example@example.com",
+      },
+    },
+    password: {
+      type: "string",
+      minLength: 8,
+      maxLength: 50,
+      errorMessage: {
+        type: "Password must be a string",
+        minLength: "Password must be at least 8 characters long",
+        maxLength: "Password must be at most 50 characters long",
+      },
+    },
+    ocupation: {
+      type: "string",
+      minLength: 3,
+      maxLength: 50,
+      errorMessage: {
+        type: "Ocupation must be a string",
+        minLength: "Ocupation must be at least 3 characters long",
+        maxLength: "Ocupation must be at most 50 characters long",
+      },
+    },
+    nameWorkPlace: {
+      type: "string",
+      minLength: 3,
+      maxLength: 50,
+      errorMessage: {
+        type: "NameWorkPlace must be a string",
+        minLength: "NameWorkPlace must be at least 3 characters long",
+        maxLength: "NameWorkPlace must be at most 50 characters long",
+      },
+    },
+    incomings: {
+      type: "number",
+      minimum: 0,
+      errorMessage: {
+        type: "Incomings must be a number",
+        minimum: "Incomings must be at least 0",
+      },
+    },
+    image: {
+      type: "string",
+      errorMessage: {
+        type: "Image must be a string",
+      },
+    },
+  },
+  required: [
+    "name",
+    "username",
+    "NoIdentification",
+    "birthdate",
+    "address",
+    "phone",
+    "email",
+    "password",
+    "ocupation",
+    "nameWorkPlace",
+    "incomings",
+  ],
+  errorMessage: {
+    required: {
+      name: "Name is required",
+      username: "Username is required",
+      NoIdentification: "NoIdentification is required",
+      birthdate: "Birthdate is required",
+      address: "Address is required",
+      phone: "Phone is required",
+      email: "Email is required",
+      password: "Password is required",
+      ocupation: "Ocupation is required",
+      nameWorkPlace: "NameWorkPlace is required",
+      incomings: "Incomings is required",
+    },
+  },
+  additionalProperties: false,
+};
+
+module.exports = schemaCreateUser
---
+++ Back/src/middlewares/AJV_validate/index.js
@@ -0,0 +1,64 @@
+const Ajv = require("ajv");
+const ajv = new Ajv({ allErrors: true, allowUnionTypes: true });
+require("ajv-errors")(ajv);
+
+// Function to validate body request
+function validateBody(schema) {
+    const validate = ajv.compile(schema);
+
+    return (req, res, next) => {
+        const valid = validate(req.body);
+
+        if(!valid) {
+            const errors = validate.errors.map(error => error.message)
+            const errorMessage = errors.join('. ')
+            return res.status(400).send({ message: errorMessage });
+        }
+
+        next();
+    }
+}
+
+// Function to validate params request
+function validateParams(schema) {
+    const validate = ajv.compile(schema);
+
+    return function (req, res, next) {
+        const valid = validate(req.params);
+
+        if (!valid) {
+            const errors = validate.errors.map(error => error.message);
+            const errorMessage = errors.join('. ');
+            return res.status(400).send({
+                result: false,
+                message: errorMessage,
+                records: ''
+            });
+        }
+
+        next();
+    };
+}
+
+// Function to validate query request
+function validateQuery(schema) {
+    const validate = ajv.compile(schema);
+
+    return function (req, res, next) {
+        const valid = validate(req.query);
+
+        if (!valid) {
+            const errors = validate.errors.map(error => error.message);
+            const errorMessage = errors.join('. ');
+            return res.status(400).send({
+                result: false,
+                message: errorMessage,
+                records: ''
+            });
+        }
+
+        next();
+    };
+}
+
+module.exports = { validateBody, validateParams, validateQuery };
