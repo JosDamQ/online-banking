8ee873ac473891b3668ef5b55b310ed4c56d6318
jgarcia-2018163 <jgarcia-2018163@kinal.edu.gt>
2024-06-17 15:42:16 -0600

    create routes for Account

--- Back/src/accounts/account.routes.js
+++ Back/src/accounts/account.routes.js
@@ -7,8 +7,21 @@ const accountController = require('./account.controller')
 
 const { isAdmin } = require('../middlewares/isAdmin')
 const { verifyToken } = require('../middlewares/loggedIn')
+const { validateBody, validateParams, validateQuery } = require('../middlewares/AJV_validate/index')
+//Schemas
+const schemaCreateAccount = require('../middlewares/AJV_validate/Account/CreateAccount')
+const schemaGetById = require('../middlewares/AJV_validate/Account/GetById')
+const schemaGetByNumber = require('../middlewares/AJV_validate/Account/GetByNumber')
+const schemaGetByUser = require('../middlewares/AJV_validate/Account/GetByUser')
+const schemaGetByMovements = require('../middlewares/AJV_validate/Account/GetByMovements')
 
 //Rutas
-router.post('/create', verifyToken, isAdmin, accountController.createAccount)
+router.post('/create', verifyToken, isAdmin, validateBody(schemaCreateAccount), accountController.createAccount)
+router.get('/getAll', verifyToken, isAdmin, accountController.getAccounts)
+router.get('/get/:id', verifyToken, isAdmin, validateParams(schemaGetById), accountController.getAccount)
+router.get('/getByNumber/:accountNumber', verifyToken, isAdmin, validateParams(schemaGetByNumber), accountController.searchAccountByNumber)
+router.get('/getByUser/:id', verifyToken, isAdmin, validateParams(schemaGetByUser), accountController.getAccountsByUser)
+router.get('/getByMovements', verifyToken, isAdmin, validateQuery(schemaGetByMovements), accountController.getByMovements)
+router.delete('/delete/:id', verifyToken, isAdmin, validateParams(schemaGetById), accountController.deleteAccount)
 
 module.exports = router
