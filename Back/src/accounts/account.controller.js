f7d03e4c88895e6276eeb6313eda927e60a2ad05
jgarcia-2018163 <jgarcia-2018163@kinal.edu.gt>
2024-06-17 15:41:54 -0600

    create apis for account

--- Back/src/accounts/account.controller.js
+++ Back/src/accounts/account.controller.js
@@ -36,3 +36,78 @@ exports.createAccount = async (req, res, next) => {
         next(err)
     }
 }
+
+exports.getAccounts = async (req, res, next) => {
+    try{
+        const accounts = await Account.find().populate('user', '-password').populate('accountType')
+        if(!accounts) return res.status(404).send({ message: 'Accounts not found' })
+        return res.send(accounts)
+    }catch(err){
+        console.error(err)
+        next(err)
+    }
+}
+
+exports.getAccount = async (req, res, next) => {
+    try{
+        const accountId = req.params.id
+        const account = await Account.findById(accountId).populate('user', '-password').populate('accountType')
+        if(!account) return res.status(404).send({ message: 'Account not found' })
+        return res.send(account)
+    }catch(err){
+        console.error(err)
+        next(err)
+    }
+}
+
+exports.searchAccountByNumber = async (req, res, next) => {
+    try{
+        const accountNumber = req.params.accountNumber
+        const account = await Account.findOne({ accountNumber: accountNumber }).populate('user', '-password').populate('accountType')
+        if(!account) return res.status(404).send({ message: 'Account not found' })
+        return res.send(account)
+    }catch(err){
+        console.error(err)
+        next(err)
+    }
+}
+
+exports.getAccountsByUser = async (req, res, next) => {
+    try{
+        const userId = req.params.id
+        const userExist = await User.findById(userId)
+        if(!userExist) return res.status(404).send({ message: 'User not found' })
+        const accounts = await Account.find({ user: userId }).populate('user', '-password').populate('accountType')
+        if(!accounts) return res.status(404).send({ message: 'client doest not have accounts' })
+        return res.send(accounts)
+    }catch(err){
+        console.error(err)
+        next(err)
+    }
+}
+
+exports.getByMovements = async (req, res, next) => {
+    try{
+        const { order = 'desc' } = req.query  
+        const accounts = await Account.find().sort({ movements: order == 'asc' ? 1 : -1 }).populate('user', '-password').populate('accountType')
+        if(!accounts) return res.status(404).send({ message: 'Accounts not found' })
+        return res.send(accounts)
+    }catch(err){
+        console.error(err)
+        next(err)
+    }
+}
+
+exports.deleteAccount = async (req, res, next) => {
+    try{
+        const accountId = req.params.id
+        const account = await Account.findById(accountId)
+        if(!account) return res.status(404).send({ message: 'Account not found' })
+        if(account.balance > 0) return res.status(400).send({ message: 'Account balance must be 0' })
+        await Account.findByIdAndDelete(accountId)
+        return res.send({ message: 'Account deleted successfully' })
+    }catch(err){
+        console.error(err)
+        next(err)
+    }
+}
